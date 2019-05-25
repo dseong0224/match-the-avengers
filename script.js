de98df3343703e3b68057a555fe18508213a6c5b
Daniel Seong <dseong0224@gmail.com>
2019-05-25 11:11:59 -0700

    added modal

--- script.js
+++ script.js
@@ -12,9 +12,31 @@ var matches = null;
 var firstCard = null;
 var secondCard = null;
 
-var max_matches = 9
+var max_matches = 9;
+
+var attempts = 0;
+var games_played = 0;
+
+
+function displayStats(){
+
+    function calculateAccuracy(total,divider) {
+        return parseInt(total/divider * 100) + "%";
+    }
+
+    var average = calculateAccuracy(matches,attempts);
+    console.log(parseInt(average))
+
+    $('.gamesPlayedCount span').text(games_played);
+    $('.attemptsCount span').text(attempts);
+    $('.accuracyPercentage span').text(average);
+
+}
 
 function handleCardClick(event) {
+
+
+
 
     $(this).find('.back').addClass('hidden');
 
@@ -25,9 +47,15 @@ function handleCardClick(event) {
         //first card cannot be clicked twice in a row
     }
     else if(firstCardClicked!== null && secondCardClicked === null){
+
         secondCardClicked = $(event.currentTarget).find(".front").css('background-image');
         secondCard = $(event.currentTarget).find('.back');
         console.log("secondcard: ",secondCardClicked);
+
+        if(firstCardClicked !== null && secondCardClicked !== null){
+            attempts++;
+            console.log("attempts: ",attempts)
+        }
 
         if(firstCardClicked === secondCardClicked){
             console.log("it's a match");
@@ -35,14 +63,26 @@ function handleCardClick(event) {
 
             if(matches < max_matches) {
                 matches += 1;
+                displayStats()
             }
             if(matches === max_matches){
                 //win condition
                 // alert("you won")
+                games_played++;
+                console.log("games played: ",games_played)
+
+                attempts = 0;
+                console.log('attempts:', attempts)
+                matches = 0;
+                console.log('matches: ', matches)
 
                 $('#modalBody').removeClass("hide");
-                
-                // $('.close').on('click',)
+
+                // this lets you restart the game:
+                // exit button selector
+                // click exit button -> hide modal (ADD HIDE CLASS TO MODAL)
+
+                //
 
             }
             console.log('match:',matches);
@@ -67,6 +107,7 @@ function handleCardClick(event) {
         }
     }
 }
+
 
 
 
