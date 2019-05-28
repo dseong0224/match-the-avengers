9dc1916aabe2c0e360aaf4836db49cb19af8c5af
Daniel Seong <dseong0224@gmail.com>
2019-05-28 06:25:57 -0700

    theme change, no feature change

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+//currently in branch 'blockRepeatClick'
+
 $(document).ready(function(){
     $('.card').click(handleCardClick);
 })
-
-
-//might have to make a conditional that restricts double clicks
-
+//add feature: make a conditional that restricts double clicks
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
@@ -13,122 +12,99 @@ var firstCard = null;
 var secondCard = null;
 
 var max_matches = 9;
-
 var attempts = 0;
 var games_played = 0;
 
 
-function displayStats(){
-
-    function calculateAccuracy(total,divider) {
-        return parseInt(total/divider * 100) + "%";
-    }
-
-    var average = calculateAccuracy(matches,attempts);
-    console.log(parseInt(average))
-
-    $('.gamesPlayedCount span').text(games_played);
-    $('.attemptsCount span').text(attempts);
-    $('.accuracyPercentage span').text(average);
-
-}
-
 function handleCardClick(event) {
 
-
-
+    // displayStats();
+    $(event.currentTarget).find('.card').addClass('unclickable')
+    //add feature: same card cannot be clicked twice in a row
 
     $(this).find('.back').addClass('hidden');
 
     if(firstCardClicked === null && secondCardClicked === null){
+
         firstCardClicked = $(event.currentTarget).find(".front").css('background-image');
-        firstCard = $(event.currentTarget).find(".back");
-        console.log("firstcard: ",firstCardClicked);
-        //first card cannot be clicked twice in a row
+        firstCard = $(event.currentTarget);
     }
     else if(firstCardClicked!== null && secondCardClicked === null){
 
         secondCardClicked = $(event.currentTarget).find(".front").css('background-image');
-        secondCard = $(event.currentTarget).find('.back');
-        console.log("secondcard: ",secondCardClicked);
+        secondCard = $(event.currentTarget);
 
         if(firstCardClicked !== null && secondCardClicked !== null){
             attempts++;
-            console.log("attempts: ",attempts)
+            $('.attemptsCount span').text(attempts);
+            //need a cover for all cards the moment second card is flipped regardless of match condition.
+            //give it a 1.5sec gap before you can click a third card.
+            // $('.container').find('.containerCover').addClass('unclickable') howwww!!???
+
+
+            if(firstCardClicked === secondCardClicked){
+
+                firstCard.find('.selectedCardCover').removeClass('hide');
+                secondCard.find('.selectedCardCover').removeClass('hide');
+
+                //add feature: flipped cards should not be clicked
+
+                if(matches < max_matches) {
+                    matches += 1;
+                }
+                if(matches === max_matches){
+
+                    attempts = 0;
+                    matches = 0;
+
+                    $('#modalBody').removeClass("hide");
+
+                    $('.exit').click(function () {
+                        $('#modalBody').addClass("hide");
+                    });
+                    $('.playAgain').click(function () {
+                        $('#modalBody').addClass("hide");
+                        $('.card').find('.back').removeClass('hidden')
+                    });
+                    attempts = 0;
+                    matches = 0;
+                    games_played++;
+                    $('.gamesPlayedCount span').text(games_played);
+                    $('div').find('.selectedCardCover').addClass('hide')
+                }
+
+                if(firstCardClicked && secondCardClicked){
+                    firstCardClicked = null; //shadows the rest of the cards when matched
+                    secondCardClicked = null; //shadows the rest of the cards when matched
+                }
+            }
+            else if(firstCardClicked !== secondCardClicked){
+                setTimeout(function(){
+                    firstCard.find(".back").removeClass('hidden');
+                    secondCard.find(".back").removeClass('hidden');
+                },1500);
+
+                if(firstCardClicked && secondCardClicked){
+                    firstCardClicked = null; //shadows the rest of the cards when matched
+                    secondCardClicked = null; //shadows the rest of the cards when matched
+                }
+            }
+
         }
 
-        if(firstCardClicked === secondCardClicked){
-            console.log("it's a match");
-            //flipped cards should not be clicked
 
-            if(matches < max_matches) {
-                matches += 1;
-                displayStats()
-            }
-            if(matches === max_matches){
-                //win condition
-                // alert("you won")
-                games_played++;
-                console.log("games played: ",games_played)
-
-                attempts = 0;
-                console.log('attempts:', attempts)
-                matches = 0;
-                console.log('matches: ', matches)
-
-                $('#modalBody').removeClass("hide");
-
-                // this lets you restart the game:
-                // exit button selector
-                // click exit button -> hide modal (ADD HIDE CLASS TO MODAL)
-
-                //
-
-            }
-            console.log('match:',matches);
-
-            if(firstCardClicked && secondCardClicked){
-                firstCardClicked = null;
-                secondCardClicked = null;
-            }
-        }
-        else if(firstCardClicked !== secondCardClicked){
-            console.log("not a match");
-            setTimeout(function(){
-                firstCard.removeClass('hidden');
-                secondCard.removeClass('hidden')
-            },1500);
-
-
-            if(firstCardClicked && secondCardClicked){
-                firstCardClicked = null;
-                secondCardClicked = null;
-            }
-        }
     }
+
+    function displayStats(){
+
+        function calculateAccuracy(total,divider) {
+            return parseInt(total/divider * 100) + "%";
+        }
+
+        var average = calculateAccuracy(matches,attempts);
+
+        $('.accuracyPercentage span').text(average);
+    }
+    displayStats();
+
 }
-
-
-
-
-
-
-
-
-// console.log('Event:', event.currentTarget);
-
-
-
-
-// jQuery reference for the first card you clicked
-
-// firstCardClicked = $(this);
-//
-// if(firstCardClicked){
-//     secondCardClicked = $(this)
-// }
-//
-// if(firstCardClicked === secondCardClicked){
-//     console.log("it's a match")
-// }
-
