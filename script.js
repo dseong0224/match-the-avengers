49bf49db9b55ef2f5ff06be6f6780270403d1313
dseong0224 <dseong0224@gmail.com>
2019-08-26 21:48:07 +0000

    added function to remove existing divs new deck of cards displayed on next round click

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ let levelFiveCardsArray = [
     "url(./assets/images/cards/blackwidow_card.jpg)",
     "url(./assets/images/cards/blackwidow_card.jpg)"
 ];
+
 let deckInPlay = levelOneCardsArray;
 
 let firstCardUrl = null;
@@ -73,11 +74,25 @@ let attempts = 0;
 let counter;
 let timePassed = 0;
 
-function startTimer(){
-    counter = setInterval(function(){
-    $("#countup").text(timePassed + "  sec");
-    timePassed += 1;
-    }, 1000);
+function startGame(deck) {
+    removePreviousDeck();
+    resetCardValues();
+    resetTimer();
+    startTimer();
+    resetStats();
+    setGameTable(deck);
+    closeModal();
+    hideStartModal();
+    $(".card").click(handleCardClick);
+    playAudio();
+}
+
+function setGameTable (deck){
+    let shuffledArray = shuffleArray(deck);
+    let gameDeck = shuffledArray.slice(0,8);
+    gameDeck = gameDeck.concat(gameDeck);
+    gameDeck = shuffleArray(gameDeck);
+    generateCardDivs(gameDeck);
 }
 
 function shuffleArray(array) {
@@ -93,7 +108,7 @@ function shuffleArray(array) {
     return array;
 }
 
-function generateCardHolders(cardsArray){
+function generateCardDivs(cardsArray){
     for (cardIndex = 0; cardIndex < cardsArray.length; cardIndex++ ) {
         let cardDiv = $('<div>').addClass('card');
         let frontDiv = $('<div>').addClass('front').css('background-image', cardsArray[cardIndex]);
@@ -102,12 +117,148 @@ function generateCardHolders(cardsArray){
         $(".deck").append(gameDeck);
     }    
 }
-function setGameTable (deck){
-    let shuffledArray = shuffleArray(deck);
-    let gameDeck = shuffledArray.slice(0,8);
-    gameDeck = gameDeck.concat(gameDeck);
-    gameDeck = shuffleArray(gameDeck);
-    generateCardHolders(gameDeck);
+function removePreviousDeck(){
+    $("div").remove('.card');
+}
+
+function startTimer(){
+    counter = setInterval(function(){
+    $("#countup").text(timePassed + "  sec");
+    timePassed += 1;
+    }, 1000);
+}
+
+function resetTimer(){
+    clearInterval(counter);
+    timePassed = 0;
+}
+
+function handleCardClick(event) {
+    if(clickable) {
+        let clickedCard = $(event.currentTarget);
+        clickedCard.toggleClass('isFlipped');
+
+        if (!firstCard) {
+            firstCard = clickedCard;
+            firstCardUrl = clickedCard.find(".front").css('background-image');
+            clickedCard.css('pointer-events', 'none');
+        } else if (!secondCard) {
+            secondCard = clickedCard;
+            secondCardUrl = clickedCard.find(".front").css('background-image');
+            clickedCard.css('pointer-events', 'none');
+            attempts++;
+            $('.attempts-count').text(attempts);
+        }
+
+        if (firstCardUrl && secondCardUrl){
+            if (firstCardUrl !== secondCardUrl) {
+            clickable = false; 
+                setTimeout( function () {
+                    firstCard.toggleClass('isFlipped');
+                    secondCard.toggleClass('isFlipped');
+                    firstCard.css('pointer-events', ''); 
+                    secondCard.css('pointer-events', '');
+                    resetCardValues();
+                    clickable = true; 
+                }, 1500);
+            } else if (firstCardUrl === secondCardUrl) {
+                totalMatches++;
+                resetCardValues();
+                clickable = false;
+                setTimeout( function () {
+                    displayGameResult()
+                    clickable = true;
+                }, 1500);
+
+            } else {
+                return
+            }
+        }
+    }
+}
+
+function displayGameResult(){
+    if(timePassed === 150 ){
+        $(".modal-attempts").text("You  made "+ attempts + " attempts");
+        $(".modal-time").text(" in " + timePassed + " seconds");
+        $(".modal-score-title").append($(".modal-score"));
+        clearInterval(counter);
+        $(".win").text("Game Over");
+        ratePlayer();
+        openModal();
+    }
+    if(totalMatches === maxMatches){
+        $(".modal-attempts").text("You  made "+ attempts + " attempts");
+        $(".modal-time").text(" in " + timePassed + " seconds");
+        $(".modal-score-title").append($(".modal-score"));
+        clearInterval(counter);
+        ratePlayer();
+        openModal();
+    }
+}
+
+function openModal(){
+    $("#popup_shadow").removeClass("hide");
+}
+
+function closeModal(){
+    $("#popup_shadow").addClass('hide')
+}
+
+function hideStartModal(){
+    $("#play-button-shadow").addClass('hide')
+}
+
+function restartGame(){
+    resetStats();
+    startTimer()
+    $("#countup").text("ready...");
+    $('.attempts-count').text(attempts);
+    $('.card').removeClass('isFlipped');
+    $('.card').css('pointer-events', '');
+    hideGameScore();
+    closeModal();
+}
+
+function hideGameScore(){
+    $(".space").addClass("shadow");
+    $(".power").addClass("shadow");
+    $(".time").addClass("shadow");
+}
+
+function resetStats(){
+    totalMatches = null;
+    attempts = 0;
+}
+
+function resetCardValues(){
+    firstCard = null;
+    firstCardUrl = null;
+    secondCard = null;
+    secondCardUrl = null;
+}
+
+function ratePlayer(){
+    if(totalMatches === maxMatches){
+        if (timePassed < 60){
+            $(".space").removeClass("shadow");
+            $(".power").removeClass("shadow");
+            $(".time").removeClass("shadow");
+            $(".win").text("Wakanda Forever");
+            deckInPlay = levelTwoCardsArray;
+        } else if(timePassed < 90){
+            $(".space").removeClass("shadow");
+            $(".power").removeClass("shadow");
+            $(".win").text("You're getting the hand of it");
+            deckInPlay = levelTwoCardsArray;
+        } else if (timePassed < 120){
+            $(".space").removeClass("shadow");
+            $(".win").text("Maybe try again");
+            deckInPlay = levelOneCardsArray;
+        } else {
+            return;
+        }
+    }
 }
 
 function playAudio(){
@@ -124,174 +275,4 @@ function playAudio(){
         cardClickSoundEffect.play();
     });
 }
-
-function startGame(deck) {
-    firstCardUrl = null;
-    secondCardUrl = null;
-    totalMatches = null;
-    
-    firstCard = null;
-    secondCard = null;
-    
-    maxMatches = 8;
-    attempts = 0;
-    
-    timePassed = 0;
-
-    setGameTable(deck);
-    $(".card").click(handleCardClick);
-    playAudio();
-}
-
-function hideStartModal(){
-    $("#play-button-shadow").addClass('hide')
-}
-
-function handleCardClick(event) {
-
-if(clickable) {
-    let clickedCard = $(event.currentTarget);
-    clickedCard.toggleClass('isFlipped');
-
-    if (!firstCard) {
-        firstCard = clickedCard;
-        firstCardUrl = clickedCard.find(".front").css('background-image');
-        clickedCard.css('pointer-events', 'none');
-    } else if (!secondCard) {
-        secondCard = clickedCard;
-        secondCardUrl = clickedCard.find(".front").css('background-image');
-        clickedCard.css('pointer-events', 'none');
-        attempts++;
-        $('.attempts-count').text(attempts);
-    }
-
-    if (firstCardUrl && secondCardUrl){
-        if (firstCardUrl !== secondCardUrl) {
-        clickable = false; 
-            setTimeout( function () {
-                firstCard.toggleClass('isFlipped');
-                secondCard.toggleClass('isFlipped');
-                firstCard.css('pointer-events', ''); 
-                secondCard.css('pointer-events', '');
-                firstCard = null;
-                secondCard = null;
-                firstCardUrl = null;
-                secondCardUrl = null;
-                clickable = true; 
-            }, 1500);
-        } else if (firstCardUrl === secondCardUrl) {
-            totalMatches++;
-            firstCard = null;
-            firstCardUrl = null;
-            secondCard = null;
-            secondCardUrl = null;
-            clickable = false;
-            setTimeout( function () {
-                finishGame()
-                clickable = true;
-            }, 1500);
-
-        } else {
-            return
-        }
-    }
-}
-}
-
-function finishGame(){
-    if(timePassed === 150 ){
-        $(".modal-attempts").text("You  made "+ attempts + " attempts");
-        $(".modal-time").text(" in " + timePassed + " seconds");
-        $(".modal-score-title").append($(".modal-score"));
-        clearInterval(counter);
-        $(".win").text("Game Over");
-        ratePlayer();
-        openModal();
-    }
-    if(totalMatches === 8){
-        $(".modal-attempts").text("You  made "+ attempts + " attempts");
-        $(".modal-time").text(" in " + timePassed + " seconds");
-        console.log("counter: ", counter)
-        $(".modal-score-title").append($(".modal-score"));
-        clearInterval(counter);
-        ratePlayer();
-        openModal();
-    }
-}
-
-function openModal(){
-    $("#popup_shadow").removeClass("hide");
-}
-
-function closeModal(){
-    $("#popup_shadow").addClass('hide')
-}
-
-function restartGame(){
-    totalMatches = null;
-    attempts = 0;
-    firstCard = null;
-    firstCardUrl = null;
-    secondCard = null;
-    secondCardUrl = null;
-    
-    clearInterval(counter);
-    timePassed = 0;
-    startTimer()
-    $("#countup").text("ready...");
-    $('.attempts-count').text(attempts);
-    $('.card').removeClass('isFlipped');
-    $('.card').css('pointer-events', '');
-
-    $(".space").addClass("shadow");
-    $(".power").addClass("shadow");
-    $(".time").addClass("shadow");
-    closeModal();
-}
-
-function startNextRound(){
-    totalMatches = null;
-    attempts = 0;
-    firstCard = null;
-    firstCardUrl = null;
-    secondCard = null;
-    secondCardUrl = null;
-    
-    clearInterval(counter);
-    timePassed = 0;
-    startTimer()
-    $("#countup").text("ready...");
-    $('.attempts-count').text(attempts);
-    $('.card').removeClass('isFlipped');
-    $('.card').css('pointer-events', '');
-
-    $(".space").addClass("shadow");
-    $(".power").addClass("shadow");
-    $(".time").addClass("shadow");
-    closeModal();
-    deck = levelTwoCardsArray;
-    $(".next-round-button").click(startGame(deck))
-}
-
-function ratePlayer(){
-    if(totalMatches === 8){
-        if (timePassed < 60){
-            $(".space").removeClass("shadow");
-            $(".power").removeClass("shadow");
-            $(".time").removeClass("shadow");
-            $(".win").text("Wakanda Forever");
-        } else if(timePassed < 90){
-            $(".space").removeClass("shadow");
-            $(".power").removeClass("shadow");
-            $(".win").text("You're getting the hand of it");
-        } else if (timePassed < 120){
-            $(".space").removeClass("shadow");
-            $(".win").text("Maybe try again");
-        } else {
-            return;
-        }
-    }
-}
-
-
 
